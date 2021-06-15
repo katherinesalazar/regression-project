b34c6a4a21af7f77e670bc3ad8f64fd1bd1a70b2
katherinesalazar <47463674+katherinesalazar@users.noreply.github.com>
2021-06-15 11:49:13 -0500

    final explore.py

--- explore.py
+++ explore.py
@@ -32,9 +32,7 @@ def explore_univariate(train, cat_vars, quant_vars):
         explore_univariate_categorical(train, var)
         print('_________________________________________________________________')
     for col in quant_vars:
-        p, descriptive_stats = explore_univariate_quant(train, col)
-        plt.show(p)
-        print(descriptive_stats)
+        plt.show()
         
 def explore_bivariate(train, target, cat_vars, quant_vars):
     for cat in cat_vars:
@@ -45,8 +43,8 @@ def explore_bivariate(train, target, cat_vars, quant_vars):
 def explore_multivariate(train, target, cat_vars, quant_vars):
     '''
     '''
-    plot_swarm_grid_with_color(train, target, cat_vars, quant_vars)
-    plt.show()
+    #plot_swarm_grid_with_color(train, target, cat_vars, quant_vars)
+    #plt.show()
     violin = plot_violin_grid_with_color(train, target, cat_vars, quant_vars)
     plt.show()
     pair = sns.pairplot(data=train, vars=quant_vars, hue=target)
@@ -105,84 +103,66 @@ def freq_table(train, cat_var):
 
 #### Bivariate
 
-def explore_bivariate_categorical(train, target, cat_var):
+def explore_bivariate(train, target, cat_vars):
+    for cat in cat_vars:
+        explore_bivariate_categorical(train, 'tax_value', cat)
+    #for quant in quant_vars:
+    #    explore_bivariate_quant(train, 'tax_value', quant)
+    plt.show()
+
+def explore_bivariate_categorical(train, target, cat_vars):
     '''
     takes in categorical variable and binary target variable, 
     returns a crosstab of frequencies
     runs a chi-square test for the proportions
     and creates a barplot, adding a horizontal line of the overall rate of the target. 
     '''
-    print(cat_var, "\n_____________________\n")
-    ct = pd.crosstab(train[cat_var], train[target], margins=True)
-    chi2_summary, observed, expected = run_chi2(train, cat_var, target)
-    p = plot_cat_by_target(train, target, cat_var)
-    
+    print(cat_vars, "\n_____________________\n")
+    ct = pd.crosstab(train[cat_vars], train[target], margins=True)
+    chi2_summary, observed, expected = run_chi2(train, cat_vars, target)
+    p = plot_cat_by_target(train, target, cat_vars)
+
     print(chi2_summary)
     print("\nobserved:\n", ct)
     print("\nexpected:\n", expected)
     plt.show(p)
     print("\n_____________________\n")
 
-def explore_bivariate_quant(train, target, quant_var):
-    '''
-    descriptive stats by each target class. 
-    compare means across 2 target groups 
-    boxenplot of target x quant
-    swarmplot of target x quant
-    '''
-    print(quant_var, "\n____________________\n")
-    descriptive_stats = train.groupby(target)[quant_var].describe()
-    average = train[quant_var].mean()
-    mann_whitney = compare_means(train, target, quant_var)
-    plt.figure(figsize=(4,4))
-    boxen = plot_boxen(train, target, quant_var)
-    swarm = plot_swarm(train, target, quant_var)
-    plt.show()
-    print(descriptive_stats, "\n")
-    print("\nMann-Whitney Test:\n", mann_whitney)
-    print("\n____________________\n")
+# def explore_bivariate_quant(train, target, quant_var):
+#     '''
+#     descriptive stats by each target class. 
+#     compare means across 2 target groups 
+#     boxenplot of target x quant
+#     swarmplot of target x quant
+#     '''
+#     print(quant_var, "\n____________________\n")
+#     #descriptive_stats = train.groupby(target)[quant_var].describe()
+#     # average = train[quant_var].mean()
+#     mann_whitney = compare_means(train, target, quant_var)
+#     plt.figure(figsize=(4,4))
+#     boxen = plot_boxen(train, target, quant_var)
+#     #swarm = plot_swarm(train, target, quant_var)
+#     #plt.show()
+#     print(descriptive_stats, "\n")
+#     print("\nMann-Whitney Test:\n", mann_whitney)
+#     print("\n____________________\n")
 
 ## Bivariate Categorical
 
-def run_chi2(train, cat_var, target):
-    observed = pd.crosstab(train[cat_var], train[target])
+def run_chi2(train, cat_vars, target):
+    observed = pd.crosstab(train[cat_vars], train[target])
     chi2, p, degf, expected = stats.chi2_contingency(observed)
     chi2_summary = pd.DataFrame({'chi2': [chi2], 'p-value': [p], 
                                  'degrees of freedom': [degf]})
     expected = pd.DataFrame(expected)
     return chi2_summary, observed, expected
 
-def plot_cat_by_target(train, target, cat_var):
-    p = plt.figure(figsize=(4,4))
-    p = sns.barplot(cat_var, target, data=train, alpha=.8, color='lightseagreen')
+def plot_cat_by_target(train, target, cat_vars):
+    p = plt.figure(figsize=(2,2))
+    p = sns.barplot(cat_vars, target, data=train, alpha=.8, color='lightseagreen')
     overall_rate = train[target].mean()
     p = plt.axhline(overall_rate, ls='--', color='gray')
     return p
-    
-
-## Bivariate Quant
-
-def plot_swarm(train, target, quant_var):
-    average = train[quant_var].mean()
-    p = sns.swarmplot(data=train, x=target, y=quant_var, color='lightgray')
-    p = plt.title(quant_var)
-    p = plt.axhline(average, ls='--', color='black')
-    return p
-
-def plot_boxen(train, target, quant_var):
-    average = train[quant_var].mean()
-    p = sns.boxenplot(data=train, x=target, y=quant_var, color='lightseagreen')
-    p = plt.title(quant_var)
-    p = plt.axhline(average, ls='--', color='black')
-    return p
-
-# alt_hyp = ‘two-sided’, ‘less’, ‘greater’
-
-def compare_means(train, target, quant_var, alt_hyp='two-sided'):
-    x = train[train[target]==0][quant_var]
-    y = train[train[target]==1][quant_var]
-    return stats.mannwhitneyu(x, y, use_continuity=True, alternative=alt_hyp)
-
 
 ### Multivariate
 
@@ -216,7 +196,7 @@ def plot_swarm_grid_with_color(train, target, cat_vars, quant_vars):
     for quant in quant_vars:
         _, ax = plt.subplots(nrows=1, ncols=cols, figsize=(16, 4), sharey=True)
         for i, cat in enumerate(cat_vars):
-            sns.swarmplot(x=cat, y=quant, data=train, ax=ax[i], hue=target, palette="Set2")
+#            sns.swarmplot(x=cat, y=quant, data=train, ax=ax[i], hue=target, palette="Set2")
             ax[i].set_xlabel('')
             ax[i].set_ylabel(quant)
             ax[i].set_title(cat)
